e2a90e93ecfde535dcbd98bf5c3c78e54c83055a
Sven Slootweg <jamsoftgamedev@gmail.com>
2014-04-27 22:42:34 +0200

    Support for range requests in download.php module, to make PDFs display instantly

--- public_html/modules/download.php
+++ public_html/modules/download.php
@@ -13,6 +13,9 @@
 
 if(!isset($_APP)) { die("Unauthorized."); }
 
+/* This prevents file corruption if something unexpected happens... */
+@error_reporting(0);
+
 try
 {
 	$document = Document::CreateFromQuery("SELECT * FROM documents WHERE `SlugId` = :SlugId", array("SlugId" => $router->uParameters[1]), 60, true);
@@ -23,12 +26,133 @@ catch (NotFoundException $e)
 	die("404 Not Found");
 }
 
+$block_size = (1024 * 1000); /* 1MB block size */
+
 $source_file = "{$cphp_config->storage_path}/$document->sFilename";
 $filesize = filesize($source_file);
 
+/* Range request processing, source: https://stackoverflow.com/a/4451376/1332715 */
+$range = false;
+
+if(isset($_SERVER['HTTP_RANGE']))
+{
+	$range = $_SERVER['HTTP_RANGE'];
+}
+elseif(function_exists("apache_request_headers") && $apache_headers = apache_request_headers())
+{
+	/* This applies to some Apache servers */
+	$headers = array();
+	
+	foreach($apache_headers as $header => $val)
+	{
+		$headers[strtolower($header)] = $val;
+	}
+	
+	if(isset($headers['range']))
+	{
+		$range = $headers['range'];
+	}
+}
+
+if($range !== false)
+{
+	/* Process range data provided */
+	list($param,$range) = explode('=',$range);
+	
+	if(strtolower(trim($param)) != 'bytes')
+	{
+		/* Range must be provided in bytes */
+		http_status_code(400);
+		die();
+	}
+	
+	$range = explode(',',$range);
+	$range = explode('-',$range[0]); // We only deal with the first requested range
+	
+	if(count($range) != 2)
+	{
+		http_status_code(400);
+		die();
+	}
+	
+	if($range[0] === "")
+	{
+		$range_end = $filesize - 1;
+		$range_start = $range_end - (int) $range[0];
+	}
+	elseif($range[1] === "")
+	{
+		$range_start = (int) $range[0];
+		$range_end = $filesize - 1;
+	}
+	else
+	{
+		$range_start = (int) $range[0];
+		$range_end = (int) $range[1];
+		
+		if ($range_end >= $filesize || (!$range_start && (!$range_end || $range_end == ($filesize - 1))))
+		{
+			$range = false;
+		}
+	}
+	
+	$range_length = $range_end - $range_start + 1;
+	
+	http_status_code(206);
+	header("Content-Range: bytes $range_start-$range_end/$filesize");
+}
+/* End range request processing */
+
 header("Content-Type: application/pdf");
-header("Content-Length: {$filesize}");
 header("Content-Transfer-Encoding: Binary"); 
 header("Content-disposition: attachment; filename=\"{$document->uOriginalFilename}\""); 
-readfile($source_file);
+header('Accept-Ranges: bytes');
+
+if($range === false)
+{
+	header("Content-Length: {$filesize}");
+	$offset = 0;
+	readfile($source_file);
+}
+else
+{
+	$handle = fopen($source_file, "rb");
+
+	$block = "";
+	$length = $block_size;
+	$block_count = ceil($filesize / $block_size);
+	
+	header("Content-Length: {$range_length}");
+	
+	$offset  = $range_start;
+	
+	if($range_length < $block_size)
+	{
+		$length = $range_length;
+	}
+	
+	for($i = 0; $i < $block_count; $i++)
+	{
+		$block = stream_get_contents($handle, $length, $offset);
+		echo($block);
+		
+		if($range !== false)
+		{
+			if($offset + $block_size > $range_end)
+			{
+				break;
+			}
+			
+			if($offset + ($block_size * 2) > $range_end)
+			{
+				$length = $range_end - ($offset + $block_size);
+			}
+		}
+		
+		$offset += $block_size;
+	}
+
+	fclose($handle);
+}
+
 die();
